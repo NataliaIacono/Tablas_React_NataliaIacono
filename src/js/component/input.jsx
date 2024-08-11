e10ab392547824a1a107d8d4d9e635fb32bd8056
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-11 11:10:10 +0200

    funciona

---
+++ src/js/component/input.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Input = ({ setMultiplicador }) => {
+    const [inputValue, setInputValue] = useState(0);
+    //const [mostrarTabla, setMostrarTabla] = useState(false);
+
+    const numeroDelInput = (event) => {
+        setInputValue(Number(event.target.value)); // para obtener el valor del input
+    };
+
+    /*const mostrarTabla = () => {
+        return numerosFijos[index] * inputValue;
+    };*/
+
+    return (
+        <div>
+            <h1>Elige la table que quieras ver</h1>
+            <input type="number" placeholder="ingresa un numero del 1 al 10" value={inputValue} onChange={numeroDelInput} />
+            <button
+                onClick={() => {
+                    setMultiplicador(inputValue);
+                }}>
+                Mostrar tabla
+            </button>
+        </div>
+    );
+};
+
+export default Input;
---
+++ src/js/component/tabla.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Tabla = ({ multiplicador } = 1) => {
+    const numerosFijos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return (
+        <div>
+            <ul>
+                {numerosFijos.map((item) => (
+                    <li key={item}>
+                        {item} x {multiplicador} = {item * multiplicador}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Tabla;
